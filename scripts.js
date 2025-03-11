9fda0a34a161cca0a6e2c0ea0d7150c1addd513d
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-03-10 22:46:27 -0500

    Diagrama Lean-Canvas

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+/*document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+});*/
+function flipCard(card) {
+    card.classList.toggle("flipped");
+}
